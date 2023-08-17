8b02be3eeacab389b9e978214005e20bd77d8eca
Felipe Simao <felipe.simao1@gmail.com>
2023-08-17 13:56:41 +0000

    first commit

---
+++ env.py
@@ -0,0 +1,7 @@
+import os
+
+os.environ["DATABASE_URL"] = "postgres://rklogggl:T9FWaMRoas_5AVUupyTgE6wD9ecPaAcd@trumpet.db.elephantsql.com/rklogggl"
+
+os.environ["SECRET_KEY"] = "fms9559"
+
+os.environ["CLOUDINARY_URL"] = "cloudinary://127934888167651:JQzWS9hfozjUlXEhkejFbr48AMs@dtuxn4kik"
